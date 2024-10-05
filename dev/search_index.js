4050f5a1d876521583122521d712ef6762b4959e
Documenter.jl <documenter@juliadocs.github.io>
2024-10-05 16:47:44 +0000

    build based on b7204e3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AlgebraicArrays","category":"page"},{"location":"#AlgebraicArrays","page":"Home","title":"AlgebraicArrays","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AlgebraicArrays.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AlgebraicArrays]","category":"page"}]
+}
