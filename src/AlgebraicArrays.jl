e3dcff6ba4c5ea1984a4613ca088cb51ca13ff4f
G Jake Gebbie <ggebbie@whoi.edu>
2024-10-05 12:50:28 -0400

    import scratch work

--- src/AlgebraicArrays.jl
+++ src/AlgebraicArrays.jl
@@ -1,5 +1,161 @@
 module AlgebraicArrays
 
-# Write your package code here.
+using LinearAlgebra
 
+export VectorArray, MatrixArray
+export parent, domainsize, rangesize 
+#export CRmult
+export # export Base methods
+    size, show, vec, Matrix, *, first #,  display
+export # export LinearAlgebra methods
+    transpose, adjoint
+    
+import Base: size, show, vec, Matrix, *, first
+import LinearAlgebra: transpose, adjoint
+
+struct VectorArray{T<:Number,N,A<:AbstractArray{T,N}} <: AbstractArray{T,1}
+    data:: A
 end
+
+"""
+    VectorArray(A, rsize)
+
+Construct a `VectorArray` from an AbstractArray.
+
+# Arguments
+- `A::AbstractArray`
+- `rsize`: size of range
+"""
+VectorArray(A::AbstractArray, rsize) = reshape(A,rsize)
+
+parent(b::VectorArray) = b.data
+Base.show(b::VectorArray) = show(parent(b))
+Base.display(b::VectorArray) = display(parent(b))
+Base.size(b::VectorArray) = size(parent(b))
+Base.vec(b::VectorArray) = vec(parent(b))
+Base.getindex(b::VectorArray, inds...) = getindex(parent(b), inds...)
+
+rangesize(b::VectorArray) = size(parent(b))
+domainsize(b::VectorArray) = ()
+
+struct MatrixArray{T<:Number,
+    M,
+    N,
+    R<:AbstractArray{T,M},
+    C<:AbstractArray{R,N}} <: AbstractArray{T,2}
+    data::C
+end
+
+"""
+    MatrixArray(A,rsize,dsize)
+
+Construct a `MatrixArray` from an AbstractArray.
+
+# Arguments
+- `A::AbstractArray`
+- `rsize`: size of range
+- `dsize`: size of domain
+"""
+function MatrixArray(A::AbstractMatrix{T},rsize,dsize) where T <: Number 
+
+    M = prod(dsize)
+    N = length(rsize)
+    P = Array{Array{T,N}}(undef,dsize)
+    for j in 1:M 
+        P[j] = reshape(A[:,j],rsize)
+    end
+    return MatrixArray(P)
+end
+
+parent(A::MatrixArray) = A.data
+Base.show(A::MatrixArray) = show(parent(A))
+#Base.display(A::MatrixArray) = display(parent(A))
+#Base.size(A::MatrixArray) = size(Matrix(A))
+Base.size(A::MatrixArray) = size(parent(A))
+Base.getindex(A::MatrixArray, inds...) = getindex(parent(A), inds...) # need to reverse order?
+
+domainsize(A::MatrixArray) = size(parent(A))
+rangesize(A::MatrixArray) = size(first(parent(A)))
+
+"""
+function Matrix(P::MatrixArray{T}) where T <: Number
+"""
+function Matrix(P::MatrixArray{T}) where T <: Number
+    N = length(P) # number of columns/ outer dims
+    M = length(first(P)) # number of rows, take first inner element as example
+
+    A = Array{T}(undef,M,N)
+    if N > 1  
+        #for j in eachindex(P) # return Cartesian Index which fails on lhs
+        for j in 1:N # return Cartesian Index which fails on lhs
+            A[:,j] = P[j][:]
+        end
+    elseif N == 1
+        #for i in eachindex(first(P))
+        for i in 1:M #eachindex(first(P))
+            A[i,1] = first(P)[i] # keep it as a matrix
+        end
+    end
+    return A 
+end
+
+# a pattern for any function
+Base.transpose(P::MatrixArray) = MatrixArray( transpose(Matrix(P)), domainsize(P), rangesize(P))
+
+Base.adjoint(P::MatrixArray) = MatrixArray( adjoint(Matrix(P)), domainsize(P), rangesize(P))
+
+# function Base.transpose(P::MatrixArray)
+#     dsize = domainsize(P)
+#     rsize = rangesize(P)
+#     #A = reshape(Matrix(P),rsize,prod(
+#     return MatrixArray(Matrix(transpose(Matrix(P))), dsize, rsize)
+# end
+
+# function Base.adjoint(P::MatrixArray) 
+#     dsize = domainsize(P)
+#     rsize = rangesize(P)
+#     return MatrixArray( adjoint(Matrix(P)), dsize, rsize)
+# end
+
+# function Base.:*(A::MatrixArray, b::VectorArray)
+#     c = zero(first(A))
+#     for j in eachindex(A)
+#         c += A[j] * b[j]
+#     end
+#     return VectorArray(c)
+# end
+#slightly faster version of multiplication
+function Base.:*(A::MatrixArray, b::VectorArray)
+    C = Matrix(A) * vec(b)
+    (C isa Number) && (C = [C])
+    rowdims = size(first(A))
+    return VectorArray(reshape(C,rowdims))
+end
+
+function Base.:*(A::MatrixArray, B::MatrixArray) 
+    C = Matrix(A) * Matrix(B)
+    (C isa Number) && (C = [C])
+
+    #reshape using all of the dimensions
+    rsize = size(first(A))
+    dsize  = size(B)
+    return MatrixArray(C,rsize,dsize)
+end
+
+
+# function CRmult(A::MatrixArray{T}, B::MatrixArray{T}) where T <: Number
+#     # preallocate
+#     C = Array{Array{T}(und
+
+#         zero(first(A)
+#     c = zero(first(A))
+#     for j in eachindex(A)
+#         c += A[j] * b[j]
+#     end
+#     return VectorArray(c)
+# end
+
+# first element in MatrixArray is a VectorArray
+#Base.first(A::MatrixArray) = VectorArray(first(A))
+
+end # module AlgebraicArrays
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,73 @@ using Test
 
 @testset "AlgebraicArrays.jl" begin
     # Write your tests here.
+
+    using Revise
+    using AlgebraicArrays
+    #using ArraysOfArrays
+
+    a = randn(5,5)
+    b = VectorArray(a)
+
+
+    # make an array of arrays
+    rowdims = (4,4)
+    coldims = (5,5)
+    alldims = Tuple(vcat([i for i in rowdims],[j for j in coldims]))
+
+    C = nestedview(randn(alldims),length(coldims))
+    C[5,5][4,4]
+
+    # turn an array of arrays into a MatrixArray
+    D = MatrixArray(C)
+    D = MatrixArray(Matrix(C))
+
+    # turn a MatrixArray into an algebraic array
+    Matrix(D)
+
+    # multiplication of a MatrixArray and a VectorArray gives a VectorArray
+    q = D*b
+
+    rsize = (4,4)
+    dsize = (5,5)
+    MatrixArray(Matrix(D),rsize, dsize) # doesn't work 
+
+    # matrix-matrix multiplication
+    rowdims = (3,3)
+    coldims = (3,3)
+    alldims = Tuple(vcat([i for i in rowdims],[j for j in coldims]))
+
+    E = nestedview(randn(alldims),length(coldims))
+    E[2,2][2,2]
+
+
+    F = E * E
+    F[2,2]
+
+    DT = transpose(D)
+    Ddagger = adjoint(D)
+
+
+    G = MatrixArray(Matrix(E))
+    G[2,2]
+    H = G * G
+    typeof(G)
+
+    J = MatrixArray(Matrix(G),(3,3),(3,3))
+
+    # nested_array(A::AbstdractMatrix,rangedims,domaindims)
+
+    #     # extra step for type stability
+    #     Q1 = reshape(A[:,1],size(rangedims))
+    #     P1 = DimArray(Q1, rangedims)
+
+    #     P = Array{typeof(P1)}(undef,size(domaindims))
+    #     for j in eachindex(P)
+    #         Q = reshape(A[:,j],size(rangedims))
+    #         P[j] = DimArray(Q, rangedims)
+    #     end
+    #     return DimArray(P, domaindims)
+    # end
+
+
 end
