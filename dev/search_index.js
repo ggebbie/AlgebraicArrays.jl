ce76f5eb9a46085dacdb7f887a501addb83900f3
Documenter.jl <documenter@juliadocs.github.io>
2026-02-01 20:48:05 +0000

    build based on e279fc8

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#AlgebraicArrays","page":"Home","title":"AlgebraicArrays","text":"Documentation for AlgebraicArrays.\n\n","category":"section"},{"location":"#AlgebraicArrays.find_va-Tuple{Base.Broadcast.Broadcasted}","page":"Home","title":"AlgebraicArrays.find_va","text":"A = find_va(As) returns the first VectorArray among the arguments.\n\n\n\n\n\n","category":"method"}]
+[{"location":"#AlgebraicArrays","page":"Home","title":"AlgebraicArrays","text":"Documentation for AlgebraicArrays.\n\n","category":"section"}]
 }
