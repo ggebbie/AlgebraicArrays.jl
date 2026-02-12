ef34b69495a646e67ad32211e3d44b5456baec52
G Jake Gebbie <ggebbie@whoi.edu>
2026-02-12 11:25:43 -0500

    through test_DimensionalData

--- src/AlgebraicArrays.jl
+++ src/AlgebraicArrays.jl
@@ -53,7 +53,6 @@ struct AlgebraicArray{T,D,N,A <: AbstractArray{T,N}} <: AbstractArray{T,D}
         # problem: Sometimes you want a 2D array to be a MatrixArray, but this will mess it up
         # N == Dnew ? need_reshape = true : need_reshape = false  # passing algebraic data
         if need_reshape  # passing algebraic data
-            println("needs reshape")
             x2 = reshape(x, unwrap(ynew))
             return new{T,Dnew,ndims(x2),typeof(x2)}(x2,ynew)
         else
