9b08487e350ed729c240aa718094e3d78f7b7f85
G Jake Gebbie <ggebbie@whoi.edu>
2026-02-13 11:38:49 -0500

    Revert "a start"
    This reverts commit 723f37a858993e0706185fe0b3d3665dd613be7a.

--- test/test_AlgebraicArrays.jl
+++ test/test_AlgebraicArrays.jl
@@ -1,24 +1,23 @@
 @testset "constructors" begin
 
-    # size of a VectorArray
     rsize = (2,3)
-    vsize = (rsize,) # tells it to make a vector
-    @test fill(2.0,vsize) isa VectorArray  
-    @test ones(vsize) isa VectorArray
-    @test randn(vsize) isa VectorArray
-    @test zeros(vsize) isa VectorArray 
-    @test rand(vsize) isa VectorArray
+
+    @test fill(2.0,rsize,:VectorArray) isa VectorArray
+    @test ones(rsize,:VectorArray) isa VectorArray
+    @test randn(rsize,:VectorArray) isa VectorArray
+    @test zeros(rsize,:VectorArray) isa VectorArray
+    @test rand(rsize,:VectorArray) isa VectorArray
         
     # investigator makes a field with physical dimensions
     a = randn(rsize)
 
     # can immediately save it as a VectorArray for future calculations
-    b = AlgebraicArray(a, vsize)
+    b = VectorArray(a)
 
     @testset "vector broadcasting and slicing" begin
         @test b[1,:] isa VectorArray
         @test b[1:2,:] isa VectorArray
-        # @test b[:,2:end] isa VectorArray # end keyword not correct
+        @test b[:,2:end] isa VectorArray # end keyword not correct
         v = deepcopy(b)
         v[1,:] .+= 1.0 
         @test isapprox( sum(v-b), rsize[2])
@@ -32,23 +31,19 @@
     end
         
     # internal algorithms must be able to turn into a vector, then bring it back to VectorArray
-    # c = AlgebraicArray(a, size) 
-    # @test a == c    
+    c = AlgebraicArray(vec(a), rsize)
+    @test a == c    
 
     # test `similar`
-    @test similar(b) isa VectorArray
+    @test similar(c) isa VectorArray
 
     # custom broadcasting
-    @test all(abs.(b) .> 0)
+    @test all(abs.(c) .> 0)
         
     # # make an array of arrays
     rsize = (1,2)
     dsize = (2,1)
-    msize = (dsize, rsize)
-    mdata = fill(2.0, AlgebraicArrays.unwrap(msize))
-    C = AlgebraicArray(mdata, msize) 
-    D = randn(msize)
-
+    D = randn(rsize,dsize,:MatrixArray)
     @test !endomorphic(D)
     @test !(diag(D) isa VectorArray)
 
@@ -57,27 +52,28 @@
 
     funks = [:randn,:zeros,:ones]
     for fnk in funks
+        #J = randn(rsize,rsize,:MatrixArray)
         rsize = (1,2)
-        msize = (rsize, rsize)
-        J = @eval $fnk($msize)
+        #J = @eval $fnk((1,2),(1,2),:MatrixArray)
+        J = @eval $fnk($rsize, $rsize,:MatrixArray)
         @test endomorphic(J) 
         @test diag(J) isa VectorArray
-        id = rand(1:size(J,1))
-        @test diag(J)[id] == J[id,id]
+        id = rand(1:prod(rsize))
+        @test diag(J)[id] == J[id][id]
     end
 
     #fill
-    J = fill(1, msize)
+    J = fill(1, rsize, rsize,:MatrixArray)
     @test endomorphic(J) 
     @test diag(J) isa VectorArray
-    id = rand(1:size(J,1))
+    id = rand(1:prod(rsize))
     @test diag(J)[id] == J[id][id]
 
     # internal algorithms must be able to turn into a matrix, then bring it back to a `MatrixArray`
-    # turn a MatrixArray back into an array of arrays: still true?
-    # E = AlgebraicArray(Matrix(D),rsize,dsize)
-    # @test D == E 
-    # @test similar(D) isa MatrixArray
+    # turn a MatrixArray back into an array of arrays
+    E = AlgebraicArray(Matrix(D),rsize,dsize)
+    @test D == E 
+    @test similar(D) isa MatrixArray
         
     @testset "matrix slicing" begin
         @test D[1] isa VectorArray
@@ -112,8 +108,7 @@
 
         rsize = (3,4)
         dsize = (2,3)
-        msize = (dsize, rsize)
-        
+
         q = randn(dsize,:VectorArray) #VectorArray(randn(dsize))
         qT = transpose(q)
         # same type than q, but type instability in code
