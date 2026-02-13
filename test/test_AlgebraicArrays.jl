67ad0b2b1a5c84295fcadcbeb2daa5e6b99c6e94
G Jake Gebbie <ggebbie@whoi.edu>
2026-02-13 11:38:48 -0500

    Revert "two diff indexing possibilities"
    This reverts commit 8f206cc1c7ff02c76a4265b6332e8bd12100d708.

--- test/test_AlgebraicArrays.jl
+++ test/test_AlgebraicArrays.jl
@@ -15,31 +15,16 @@
     # can immediately save it as a VectorArray for future calculations
     b = AlgebraicArray(a, vsize)
 
-    # what if dims don't match
-    c = AlgebraicArray(vec(a), vsize)
-
-    @test b == c
-
     @testset "vector broadcasting and slicing" begin
-        # use physical indices
-        @test b[(1,:)] isa VectorArray
-        @test b[(1:2,:)] isa VectorArray
-
-        # currently failing
-        @test b[:,2:end] isa VectorArray # end keyword not correct
-        @test b[:,2:3] isa VectorArray # end keyword not correct
-
+        @test b[1,:] isa VectorArray
+        @test b[1:2,:] isa VectorArray
+        # @test b[:,2:end] isa VectorArray # end keyword not correct
         v = deepcopy(b)
-
-        v[(1,:)] .+= 1.0         # currently failing
-        v[1,:] .+= 1.0         # currently failing
-        v[1] .+= 1.0     # currently failing
+        v[1,:] .+= 1.0 
         @test isapprox( sum(v-b), rsize[2])
 
         v = deepcopy(b)
-        v[1,:] = v[1,:] .+ 1.0 # works
-        v[(1,:)] = v[(1,:)] .+ 1.0 # fails
-        v[1] = v[1] .+ 1.0 # works
+        v[1,:] = v[1,:] .+ 1.0 
         @test isapprox(sum(v-b), rsize[2])
 
         # iteration
@@ -54,16 +39,16 @@
     @test similar(b) isa VectorArray
 
     # custom broadcasting
-    @test all(abs.(b) .> 0) # careful that it doesn't change type
+    @test all(abs.(b) .> 0)
         
+    # # make an array of arrays
     rsize = (1,2)
     dsize = (2,1)
     msize = (dsize, rsize)
     mdata = fill(2.0, AlgebraicArrays.unwrap(msize))
     C = AlgebraicArray(mdata, msize) 
-    D = fill(2.0, msize)
+    D = randn(msize)
 
-    @test C == D
     @test !endomorphic(D)
     @test !(diag(D) isa VectorArray)
 
@@ -81,12 +66,12 @@
         @test diag(J)[id] == J[id,id]
     end
 
-    #fil
-    J = fill(1, ((2,1),(2,1)))
+    #fill
+    J = fill(1, msize)
     @test endomorphic(J) 
     @test diag(J) isa VectorArray
     id = rand(1:size(J,1))
-    @test diag(J)[id] == J[id,id]
+    @test diag(J)[id] == J[id][id]
 
     # internal algorithms must be able to turn into a matrix, then bring it back to a `MatrixArray`
     # turn a MatrixArray back into an array of arrays: still true?
@@ -95,35 +80,32 @@
     # @test similar(D) isa MatrixArray
         
     @testset "matrix slicing" begin
-        @test D[1] isa Number
-        @test D[2,1] isa Number
-        @test D[1:2,1] isa VectorArray
-        @test D[1:2,:] isa MatrixArray # this one is not right, should not be VectorArray
+        @test D[1] isa VectorArray
+        @test D[2,1] isa VectorArray
+        @test D[1:2,1] isa MatrixArray
+        @test D[1:2] isa MatrixArray
 
         D2 = deepcopy(D)
-
-        D2[(:,:),(1,2)] .+= 1.0 # currently failing
+        D2[2,1] .+= 1.0 
         @test all(isapprox.(sum(D2-D), 1.0))
 
         # iteration uses CartesianIndices not linear indices, would need to set `iterate` function 
         # @test eachindex(D) == Base.OneTo(prod(size(b)))
 
-        @test D[(2,1),(1,1)] isa Number
-        @test D[(2,1),(:,:)] isa  VectorArray # row vector but Julia returns regular vector
-
-        # need to restate this onee
-        # @test all(isapprox.(transpose(Matrix(D)[1,:]), Matrix(rowvector(D,1,1))))
+        @test D[2,1][1,1] isa Number
+        @test rowvector(D,1,1) isa MatrixArray
+        @test all(isapprox.(transpose(Matrix(D)[1,:]), Matrix(rowvector(D,1,1))))
             
         # setindex!
-        D[(2,1),(1,1)] = 0.0 # failing
+        D[2,1][1,1] = 0.0
         # set columns to be equal
-        D[(:,:),(1,2)] .= D[(:,:),(1,1)] # failing
+        D[2,1] .= D[1,1]
         # set rows to be equal
-        D[(2,1)(:,:)] .= D[(1,1),(:,:)] # failing
+        #rowvector(D,1) .= 0.0 # fails, use comprehension instead
+            
     end
         
-    # now possible to broadcast to nested array
-    # but type changes
+    # not possible to broadcast to nested array
     F = real(D)
         
     @testset "*,+,-,/,\\ and all that" begin
@@ -132,7 +114,7 @@
         dsize = (2,3)
         msize = (dsize, rsize)
         
-        q = randn(msize); #dsize,:VectorArray) #VectorArray(randn(dsize))
+        q = randn(dsize,:VectorArray) #VectorArray(randn(dsize))
         qT = transpose(q)
         # same type than q, but type instability in code
         qTT = transpose(qT)
